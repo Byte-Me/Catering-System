0f9b84f18365c1e61ac4279eeb021d5d2893644f
olekau <olekau@student.hist.no>
2016-03-03 14:16:29 +0100

    Startet testing av GUI

---
+++ src/GUI/Login.java
@@ -0,0 +1,11 @@
+package GUI;
+
+import javax.swing.*;
+
+/**
+ * Created by olekristianaune on 03.03.2016.
+ */
+public class Login {
+    private JTextField brukernavnTextField;
+    private JPasswordField passordPasswordField;
+}
