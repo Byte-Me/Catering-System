b07342e9e0dc691210882d6819d4b008a339e05a
evendalen <even.dalen@live.no>
2016-03-15 17:59:26 +0100

    Laget grafer av ordre til statistikk

--- src/main/java/Delivery/TravelingSalesman.java
+++ src/main/java/Delivery/TravelingSalesman.java
@@ -164,12 +164,17 @@ public class TravelingSalesman {
 
     //Returns null if there is an issue with geocoding adresses. This needs too be handled elsewhere.
     public ArrayList<double[]> createPositionsArray(ArrayList<String> adresses){
-
+        ArrayList<String> fix = new ArrayList<String>();
+        for(String a : adresses){
+            if (!fix.contains(a)) {
+                fix.add(a);
+            }
+        }
         ArrayList<double[]> out = new ArrayList<double[]>();
 
-        for (int i = 0; i < adresses.size(); i++) {
+        for (int i = 0; i < fix.size(); i++) {
             try {
-                out.add(geoCoder(adresses.get(i), i));
+                out.add(geoCoder(fix.get(i), i));
             }
             catch (Exception e){
                 System.err.println("Issue with GeoCoding: " + adresses.get(i));
